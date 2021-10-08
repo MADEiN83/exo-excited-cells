231525075b1cbeba2f83180690a4c27979e8106e
MADEiN83 <anthony.distefano@hotmail.fr>
2021-10-08 08:49:10 +0200

    feat: resolve algo + clean

--- src/__spec__/index.spec.ts
+++ src/__spec__/index.spec.ts
@@ -14,4 +14,16 @@ describe("ExcitedCells tests", () => {
     expect(output).toBeDefined();
     expect(output).toEqual([1, 0, 1, 1]);
   });
+
+  it("should not throw any exceptions when passed arguments was undefined", () => {
+    const output = service.proceed();
+    expect(output).toBeDefined();
+    expect(output).toEqual([]);
+  });
+
+  it("should return `[1, 0, 1, 0]` when steps=1 and state=[1, 0, 1, 1]", () => {
+    const output = service.proceed(1, [1, 0, 1, 1]);
+    expect(output).toBeDefined();
+    expect(output).toEqual([1, 0, 1, 0]);
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,74 @@
-class ExcitedCells {
-  proceed = (steps: number, series: number[]): number[] => {
-    if (this.stepsEqualsZero(steps)) {
-      return series;
+enum State {
+  QUIET,
+  EXCITED,
+}
+
+class Excitedstate {
+  private steps: number;
+  private state: number[];
+
+  proceed = (steps: number = 0, state: number[] = []): number[] => {
+    this.steps = steps;
+    this.state = state;
+
+    if (this.stepsEqualsZero()) {
+      return state;
     }
 
-    return [];
+    const arrayOfCounter = state.map(this.checkExcitedNeighbors);
+    const newComputedCellsState = this.computeNewCellStates(arrayOfCounter);
+
+    return newComputedCellsState;
   };
 
   /**
    * Utilities.
    */
-  private stepsEqualsZero = (steps: number): boolean => !steps;
+  private stepsEqualsZero = (): boolean => !this.steps;
+
+  private checkExcitedNeighbors = (_, index): number => {
+    const cellPositions = this.computeAllCellPositions(index);
+    const counter = cellPositions.reduce(this.countExcitedNeighbors, 0);
+    return counter;
+  };
+
+  private computeAllCellPositions = (index: number): number[] => {
+    const { state } = this;
+    const positions = {
+      first: 0,
+      last: state.length - 1,
+      next: index + 1,
+      previous: index - 1,
+    };
+
+    const isFirstCell = index === positions.first;
+    const isLastCell = index === positions.last;
+    let cellPositions: number[] = [];
+
+    if (isFirstCell) {
+      cellPositions = [positions.last, positions.next];
+    } else if (isLastCell) {
+      cellPositions = [positions.previous, positions.first];
+    } else {
+      cellPositions = [positions.previous, positions.next];
+    }
+
+    return cellPositions;
+  };
+
+  private countExcitedNeighbors = (
+    counter: number,
+    cellIndex: number
+  ): number => {
+    this.state[cellIndex] === State.EXCITED && counter++;
+    return counter;
+  };
+
+  private computeNewCellStates = (arrayOfCounter: number[]) => {
+    return arrayOfCounter.map((value) =>
+      value === 1 ? State.EXCITED : State.QUIET
+    );
+  };
 }
 
-export default ExcitedCells;
+export default Excitedstate;
